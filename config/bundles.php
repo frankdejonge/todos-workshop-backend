07bf8f6aeabc13bd7469cbf58656a57043df9522
Frank de Jonge <info@frenky.net>
2018-06-18 21:56:30 +0200

    Update

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
